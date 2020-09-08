326b3db4fbfbd6a8ddb34343a6547ea0f63ccc46
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-08 21:41:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
 # lib/introduction.rb 
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+
